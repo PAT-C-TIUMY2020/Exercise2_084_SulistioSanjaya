e1b581188f8c21698bed5c0b972c33cf2ccb8f49
Sulistio Sanjaya <56523212+SulistioSanjaya20@users.noreply.github.com>
2020-12-25 00:05:36 +0700

    Add project files.

---
+++ ClientRestB_084/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientRestB_084
+{
+    public partial class Form1 : Form
+    {
+        ClassData classData = new ClassData();
+        public Form1()
+        {
+            InitializeComponent();
+            //TampilData();
+        }
+        
+        public void SearchData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1908/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+            string nim = textBoxNIM.Text;
+            if (nim == null || nim == "")
+            {
+                dataGridView1.DataSource = data;
+            }
+            else
+            {
+                var item = data.Where(x => x.nim == textBoxNIM.Text).ToList();
+
+                dataGridView1.DataSource = item;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxNama.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            textBoxNIM.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+            textBoxProdi.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+            textBoxAngkatan.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+
+            buttonUpdate.Enabled = true;
+        }
+
+        private void buttonTotal_Click(object sender, EventArgs e)
+        {
+            var json = new WebClient().DownloadString("http://localhost:1908/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+            int length = data.Count();
+            txtJumlah.Text = Convert.ToString(length);
+        }
+        
+
+        private void txtJumlah_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (textBoxNIM.Text != "" &&
+                textBoxNama.Text != "" &&
+                textBoxProdi.Text != "" &&
+                textBoxAngkatan.Text != "")
+            {
+                if (textBoxNIM.Text.Length <= 12 &&
+                textBoxAngkatan.Text.Length <= 4 &&
+                textBoxProdi.Text.Length <= 30 &&
+                textBoxNama.Text.Length <= 20)
+                {
+                    try
+                    {
+                        Mahasiswa mhs = new Mahasiswa();
+                        mhs.nim = textBoxNIM.Text;
+                        mhs.nama = textBoxNama.Text;
+                        mhs.prodi = textBoxProdi.Text;
+                        mhs.angkatan = textBoxAngkatan.Text;
+
+                        ClassData classData = new ClassData();
+                        classData.updateDatabase(mhs);
+                        MessageBox.Show("Data successfuly updated");
+                       
+                        dataGridView1.DataSource = classData.getAllData();
+                    }
+                    catch
+                    {
+                        label5.Text = "Server Error";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please check your data");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please check your data");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    ClassData classData = new ClassData();
+                    classData.deleteMahasiswa(textBoxNIM.Text);
+                    dataGridView1.DataSource = classData.getAllData();
+                    MessageBox.Show("Data successfuly deleted");
+                }
+                catch (Exception ex)
+                {
+                   label5.Text = "Server Error";
+                }
+            }
+        }
+
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.DataSource = classData.getAllData();
+            }
+            catch
+            {
+                label5.Text = "Server error";
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ServiceRest_b/ITI_UMY.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiceRest_b
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITI_UMY
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)] // Untuk membuat slash, selalu relative
+        List<Mahasiswa> GetAllMahasiswa(); // Mendapatkan kumpulan mahasiswa / seluruh data mahasiswa
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Mahasiswa/nim=(nim)", ResponseFormat = WebMessageFormat.Json)]
+        Mahasiswa GetMahasiswaByNIM(string nim);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json)]
+        string CreateMahasiswa(Mahasiswa mhs);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "UpdateMahasiswaByNIM", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string UpdateMahasiswaByNIM(Mahasiswa mhs);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "DeleteMahasiswa/{nim}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string DeleteMahasiswa(string nim);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Mahasiswa3", ResponseFormat = WebMessageFormat.Json)]
+        string CountMahasiswa();
+    }
+    [DataContract]
+    public class Mahasiswa
+    {
+        private string _nama, _nim, _prodi, _angkatan; //_ adalah konvensi atau kesepakatan //variabel lokal
+        [DataMember(Order = 1)] // mengirim data untuk mengurutkan
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+
+        [DataMember(Order = 2)]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+
+        [DataMember(Order = 3)]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+
+        [DataMember(Order = 4)]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+}
